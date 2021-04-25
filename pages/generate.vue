c9ba1500dd1a1432ce058799988329855d95d78d
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-04-25 08:19:16 +0700

    add api

---
+++ pages/generate.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>   
+    <!-- <vue-qrcode :value="data[0]._id" width=300 /> -->
+    <vue-qrcode v-if="data != null" :value="data[1]._id" :width="300" />
+    </div>
+</template>
+<script>
+import VueQrcode from 'vue-qrcode'
+
+export default {
+    name:"generate",
+    data(){
+        return{
+            data:null
+        }
+    },
+    components: {
+        VueQrcode,
+    },
+    async mounted(){
+      var data = await this.$axios.$get("http://localhost:4000/api/book")
+      this.data = data
+  },
+
+}
+</script>
--- pages/url.vue
+++ pages/url.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="w-1/2">
-    <p class="decode-result">Last result: <b>{{ result }}</b></p>
+  <div>
+    <div class="flex w-screen">
+      <div class="w-1/2">
 
-    <qrcode-stream :camera="camera" @decode="onDecode" @init="onInit">
-      <div v-if="validationSuccess" class="validation-success">
-        This is a URL
-      </div>
+      <p class="decode-result">Last result: <b>{{ result }}</b></p>
 
-      <div v-if="validationFailure" class="validation-failure">
-        This is NOT a URL!
-      </div>
+      <qrcode-stream :camera="camera" @decode="onDecode" @init="onInit">
+        <div v-if="validationSuccess" class="validation-success">
+          {{data[data.length]}}
+        </div>
 
-      <div v-if="validationPending" class="validation-pending">
-        Long validation in progress...
+        <div v-if="validationFailure" class="validation-failure">
+          Book not found
+        </div>
+
+        <div v-if="validationPending" class="validation-pending">
+          Long validation in progress...
+        </div>
+      </qrcode-stream>
       </div>
-    </qrcode-stream>
+      <div class="w-1/3" v-if="data != null">
+          <div class="flex justify-between" v-for="(data,index) in data" :key="data.index">
+            <h3>{{data}}</h3>
+            <button @click="deleteBook(index)">{{index}}</button>
+          </div>
+        
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,9 +38,9 @@ export default {
       isValid: undefined,
       camera: 'auto',
       result: null,
+      data:[]
     }
   },
-
   computed: {
     validationPending () {
       return this.isValid === undefined
@@ -43,7 +55,6 @@ export default {
       return this.isValid === false
     }
   },
-
   methods: {
 
     onInit (promise) {
@@ -56,16 +67,26 @@ export default {
       this.isValid = undefined
     },
 
+    deleteBook(index){  
+      this.data.splice(index,1)
+    },
+
     async onDecode (content) {
       this.result = content
       this.turnCameraOff()
-
-      // pretend it's taking really long
-      await this.timeout(3000)
-      this.isValid = content.startsWith('http')
+        // pretend it's taking really long
+        // await this.timeout(1000)[./]
+      var data = await this.$axios.$get(`http://localhost:4000/api/book/${content}`)
+      if (data != null){
+        this.data.push(data.name)
+        this.isValid = true
+      }else{
+        this.isValid = false
+      }
+      // this.isValid = content.startsWith('http')
 
       // some more delay, so users have time to read the message
-      await this.timeout(2000)
+      // await this.timeout(1000)
 
       this.turnCameraOn()
     },
