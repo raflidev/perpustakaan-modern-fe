d116dc1a9184cac96eb370196021e59951a395e3
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-04-27 23:28:24 +0700

    add register

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -10,22 +10,23 @@
                 <div class="my-5 space-y-4">
                   <div>
                     <label for="username">Username</label>
-                    <input type="username" name="username" placeholder="username" id="" class="rounded-md border-blue-400 border w-full p-2">
+                    <input type="username" v-model="username" name="username" placeholder="username" id="" class="rounded-md border-blue-400 border w-full p-2">
                   </div>
                   <div>
                     <label for="email">Email</label>
-                    <input type="email" name="email" placeholder="email" id="" class="rounded-md border-blue-400 border w-full p-2">
+                    <input type="email" v-model="email" name="email" placeholder="email" id="" class="rounded-md border-blue-400 border w-full p-2">
                   </div>
                   <div>
                     <label for="password">Password</label>
-                    <input type="password" name="password" placeholder="password" id="" class="rounded-md border-blue-400 border w-full p-2">
+                    <input type="password" v-model="password" name="password" placeholder="password" id="" class="rounded-md border-blue-400 border w-full p-2">
                   </div>
                   <div>
                     <label for="comfirm_password">Comfirm password</label>
-                    <input type="comfirm_password" name="comfirm_password" placeholder="Comfirm password" id="" class="rounded-md border-blue-400 border w-full p-2">
+                    <input type="password" v-model="confirm_password" name="comfirm_password" placeholder="Comfirm password" id="" class="rounded-md border-blue-400 border w-full p-2">
                   </div>
                 </div>
-                  <button type="submit" class="w-full bg-blue-400 rounded-md p-2 font-medium">Register</button>
+                  <button v-if="(password != null && confirm_password != null) && password == confirm_password" type="submit" @click="tryRegis()" class="w-full bg-blue-400 rounded-md p-2 font-medium">Register</button>
+                  <button v-else class="w-full bg-gray-400 rounded-md p-2 font-medium" disabled>Register</button>
               </div>
           </div>
       </div>
@@ -34,7 +35,33 @@
 
 <script>
 export default {
-
+name:"register",
+data(){
+  return{
+    full_name:null,
+    email:null,
+    username:null,
+    password:null,
+    confirm_password:null
+  }
+},
+methods:{
+  tryRegis(){
+    this.$axios.$post("http://localhost:4000/api/user", {
+      full_name:this.full_name,
+      email:this.email,
+      username:this.username,
+      password:this.password,
+      valid:false,
+    }).then(()=>{
+      this.full_name=null,
+      this.email=null,
+      this.username=null,
+      this.password=null,
+      this.confirm_password=null
+    })
+  }
+}
 }
 </script>
 
